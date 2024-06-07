f3a4e79fdb8e2f7d34cae9c9dcf95f29a34847ed
Sergei Kamianetskii <i@ksergey.ru>
2024-06-07 13:32:07 +0300

    upd calc multi model

--- src/main/java/org/demo/summary03_20240607/code/сalc/App.java
+++ src/main/java/org/demo/summary03_20240607/code/сalc/App.java
@@ -1,5 +1,6 @@
 package org.demo.summary03_20240607.code.сalc;
 
+import org.demo.summary03_20240607.code.сalc.model.MultiModel;
 import org.demo.summary03_20240607.code.сalc.model.SumModel;
 import org.demo.summary03_20240607.code.сalc.presenter.Presenter;
 import org.demo.summary03_20240607.code.сalc.view.ConsoleView;
@@ -8,6 +9,7 @@ public class App {
     public static void run() {
         Presenter p = new Presenter(new SumModel(), new ConsoleView());
         p.buttonClick();
+        p = new Presenter(new MultiModel(), new ConsoleView());
         p.buttonClick();
         p.buttonClick();
         p.buttonClick();
---
+++ src/main/java/org/demo/summary03_20240607/code/сalc/model/MultiModel.java
@@ -0,0 +1,8 @@
+package org.demo.summary03_20240607.code.сalc.model;
+
+public class MultiModel extends CalcModel {
+    @Override
+    public int result() {
+        return x * y;
+    }
+}
