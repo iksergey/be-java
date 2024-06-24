e486c5128a0a10edef8421ca9c4253fe580dc29a
Sergei Kamianetskii <i@ksergey.ru>
2024-06-24 10:38:39 +0300

    fix

--- src/main/java/org/demo/seminar09_20240619/JavaException/CheckedException.java
+++ src/main/java/org/demo/seminar09_20240619/JavaException/CheckedException.java
@@ -1,11 +1,8 @@
 package org.demo.seminar09_20240619.JavaException;
 
-import java.io.File;
-import java.io.FileReader;
-
 public class CheckedException {
     public static void main(String[] args) {
-        File file = new File("test.txt");
-        FileReader fr = new FileReader(file);
+//        File file = new File("test.txt");
+//        FileReader fr = new FileReader(file);
     }
 }
--- src/main/java/org/demo/seminar09_20240619/JavaException/UncheckedExceptions.java
+++ src/main/java/org/demo/seminar09_20240619/JavaException/UncheckedExceptions.java
@@ -18,7 +18,6 @@ public class UncheckedExceptions {
         strNull();
         // String str = null;
         System.out.println(str.length()); // NullPointerException
-        NullPointerException
         System.out.println("end");
     }
 }
