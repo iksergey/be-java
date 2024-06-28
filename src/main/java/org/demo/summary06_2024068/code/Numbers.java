5fa75497ff351fb4a1d9854440cf050fe0a2adc6
Sergei Kamianetskii <i@ksergey.ru>
2024-06-28 11:48:54 +0300

    FunctionalInterface

---
+++ src/main/java/org/demo/summary06_2024068/code/Numbers.java
@@ -0,0 +1,55 @@
+package org.demo.summary06_2024068.code;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Numbers {
+    public static void main(String[] args) {
+        Random random = new Random(0);
+        ArrayList<Integer> li = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            li.add(random.nextInt(10));
+        }
+        System.out.println(li);
+
+        // ArrayList<Integer> res = new ArrayList<>();
+
+        // for (Integer el : li) {
+        // if (el % 2 == 0)
+        // res.add(el);
+        // }
+        ArrayList<Integer> res = where(li, e -> e % 2 == 0);
+        System.out.println(res);
+
+        res.clear();
+
+        // for (Integer el : li) {
+        // if (el % 3 == 0)
+        // res.add(el);
+        // }
+        res = where(li, e -> e % 3 == 0);
+        System.out.println(res);
+
+        res = where(li, e -> e > 5);
+        System.out.println(res);
+    }
+
+    static ArrayList<Integer> where(ArrayList<Integer> data, Condition op) {
+        ArrayList<Integer> res = new ArrayList<>();
+        for (Integer el : data) {
+            if (op.check(el))
+                res.add(el);
+        }
+        return res;
+    }
+}
+
+@FunctionalInterface
+interface Condition {
+    boolean check(int item);
+}
+
+@FunctionalInterface
+interface Foo {
+    int[] check(String[] item);
+}
---
+++ src/main/java/org/demo/summary06_2024068/code/Program.java
@@ -0,0 +1,54 @@
+package org.demo.summary06_2024068.code;
+
+public class Program {
+    public static void main(String[] args) {
+        // int result = calc(12, 34, new Sum());
+        int result = calc(12, 34, (x, y) -> x + y);
+        System.out.println(result);
+        // result = calc(12, 34, new Sub());
+        result = calc(12, 34, (x, y) -> x - y);
+        System.out.println(result);
+
+        result = calc(12, 34, new BinaryOp() {
+            @Override
+            public int action(int x, int y) {
+                return x * y;
+            }
+        });
+
+        System.out.println(result);
+
+        result = calc(12, 34, (a, b) -> a - b);
+        result = calc(12, 34, Program::act);
+
+        System.out.println(result);
+
+    }
+
+    static int calc(int a, int b, BinaryOp op) {
+        return op.action(a, b);
+    }
+
+    static int act(int t, int u) {
+        return t + u * 2;
+    }
+}
+
+@FunctionalInterface
+interface BinaryOp {
+    int action(int x, int y);
+}
+
+// class Sum implements BinaryOp {
+// @Override
+// public int action(int x, int y) {
+// return x + y;
+// }
+// }
+
+// class Sub implements BinaryOp {
+// @Override
+// public int action(int x, int y) {
+// return x - y;
+// }
+// }
